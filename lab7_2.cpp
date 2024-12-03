208d858677d0a59bfcea4eb7a76b62608186a5dd
kasidin <kasidinkhamfoo@gmail.com>
2024-12-03 16:49:43 +0700

    lab7_2.cpp completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -16,3 +16,38 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string yaname;
+    string yanum1;
+    string twonum = "";
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+    getline(cin,yaname);
+    cout << "Fahsai: Wow!!! " << yaname << " is a really cool name.\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << yaname << ": ";
+    cin >> yanum1;
+    twonum += yanum1[0];
+    twonum += yanum1[1];
+    int gear = stoi(twonum)-12;
+    string mov;
+    cout << "Fahsai: I think you may be GEAR " << gear << ". I have a free movie ticket for you.\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\nFahsai: What movie do you want to watch?\n";
+    cout << yaname << ": ";
+    getline(cin,mov);
+    getline(cin,mov);
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    string day;
+    cout << yaname << ": ";
+    getline(cin,day);
+    cout << "Fahsai: " <<day<< "....that is OK!!! I'm looking forward to watching "<<mov<< " with you.\n";
+    string ans;
+    cout << yaname << ": ";
+    cin >> ans;
+    cout << "Fahsai: 555+ see you "<<day<< ". Bye Bye " << "\\" << "(^ ^)/";
+
+}
